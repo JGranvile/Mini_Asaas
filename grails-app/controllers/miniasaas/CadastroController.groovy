c10c060d242fa455700a488b95dfe9d8b6bba241
JGranvile <joaoggranfer@gmail.com>
2024-04-24 20:54:48 -0300

    Criando 1 entregável do formulário

---
+++ grails-app/controllers/miniasaas/CadastroController.groovy
@@ -0,0 +1,32 @@
+package miniasaas
+
+import grails.converters.JSON
+
+class CadastroController {
+
+    def index() {}
+
+
+    // Ação para lidar com a submissão do formulário
+    def save() { 
+        println(params)
+        redirect(action: "index")
+    }
+
+    def fillAddress(){
+        try{
+            // def cep = request.JSON.cep
+            def cep = params.cep
+            def url = "https://viacep.com.br/ws/${cep}/json/"
+            
+            // Faça a chamada para a API ViaCEP
+            def response = new URL(url).getText()
+            render([response:response, success: true] as JSON)
+        } catch(Exception ex){
+            render([success: false] as JSON)
+        }
+        
+    }
+
+}
+    
